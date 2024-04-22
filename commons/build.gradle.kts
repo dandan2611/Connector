3d773164c06db0d4e18d7a253188f92f611fd545
Adam CAVILLON <contact@codinbox.fr>
2024-04-22 16:28:04 +0200

    feat: add commons publication

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("java")
+    `java-library`
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
+
+group = "fr.codinbox.redisconnector"
+version = "1.2.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -9,4 +15,26 @@ repositories {
 dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("org.redisson:redisson:3.28.0")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven("https://nexus.codinbox.fr/repository/maven-releases/") {
+            name = "public-releases"
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
